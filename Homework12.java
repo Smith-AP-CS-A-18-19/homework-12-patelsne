27da5ea44f9c7957cffc01ca6e585680a44bd236
Sneha Patel <Supersneha@patelsne-19-sp3.accounts.berkeleyprep.org>
2019-01-29 11:03:35 -0500

    yes it is v late

--- Homework12.java
+++ Homework12.java
@@ -11,14 +11,15 @@ public class Homework12 {
 	 * variables. The constructor should create and save an
 	 * ArrayList of RectangularShape values
 	 */
+	 private ArrayList<RectangularShape> shapes;
 	public Homework12() {
-
+		shapes = new ArrayList<RectangularShape>();
 	}
 
 	/* Adds the parameter s to the ArrayList
 	 */
 	public void addShape(RectangularShape s) {
-
+		shapes.add(s);
 	}
 
 	/* Returns the number of RectangularShape objects
@@ -26,7 +27,13 @@ public class Homework12 {
 	 * parameter r
 	 */
 	public int problem1(Rectangle2D r) {
-
+		int ans = 0;
+		for(int i = 0; i < shapes.size(); i++){
+			if(shapes.get(i).intersects(r)){
+				ans++;
+			}
+		}
+		return ans;
 	}
 
 	/* Returns the number of RectangularShape objects
@@ -34,21 +41,40 @@ public class Homework12 {
 	 * parameter p
 	 */
 	public int problem2(Point2D p) {
-
+		int ans = 0;
+		for(int i = 0; i < shapes.size(); i++){
+			if(shapes.get(i).contains(p) == false){
+				ans++;
+			}
+		}
+		return ans;
 	}
 
 	/* Returns the number of Ellipse2D objects that
 	 * are in the ArrayList
 	 */
 	public int problem3() {
-
+		int size = 0;
+		Ellipse2D ell = new Ellipse2D.Double();
+		for (int i = 0; i < shapes.size(); i++){
+			RectangularShape r = shapes.get(i);
+			if (r.getClass() == ell.getClass()){
+				size++;
+			}
+		}
+		return size;
 	}
 
 	/* Returns true if any of the Rectangle2D objects
 	 * in the ArrayList intersects the parameter Line2D
 	 */
 	public boolean problem4(Line2D l) {
-
+		for (int i = 0; i < shapes.size(); i++){
+			if(shapes.get(i).contains(l.getX1(), l.getY1())){
+				return true;
+			}
+		}
+		return false;
 	}
 
 	/* Return the total area of the RectangularShape objects in
@@ -60,7 +86,21 @@ public class Homework12 {
 	 * counts for both (i.e. do not account for intersection)
 	 */
 	public double problem5() {
-
+		Rectangle2D r = new Rectangle2D.Double();
+		Ellipse2D e = new Ellipse2D.Double();
+		double totalArea = 0;
+		for (int i = 0; i < shapes.size(); i++){
+			if (shapes.get(i).getClass() == r.getClass()){
+				double w = shapes.get(i).getWidth();
+				double h = shapes.get(i).getHeight();
+				double area = w * h;
+				totalArea += area;
+			}
+			if (shapes.get(i) instanceof Ellipse2D){
+				totalArea += Math.PI * ((shapes.get(i).getHeight()/2) * (shapes.get(i).getWidth()/2));
+			}
+		}
+		return totalArea;
 	}
 
 	public static void main(String[] args) {
